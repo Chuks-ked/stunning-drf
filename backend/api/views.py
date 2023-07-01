35788ee49b79234c841c3da9260be981f6c9ad7a
Chuks <ikedichukwubuikem@gmail.com>
2023-07-01 15:59:02 +0100

    serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,19 @@
-import json
-from django.http import JsonResponse
+from products.models import Product
+from products.serializers import ProductSerializer
 
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    # print(dir(request))
-    # request.body
-    body = request.body
+     
+    instance = Product.objects.all().order_by('?').first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
+    if instance:
+        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price', ])
+        data = ProductSerializer(instance).data
 
-    print(data)
-    return JsonResponse(data)
+    return Response(data)
