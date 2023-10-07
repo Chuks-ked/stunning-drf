78ed9008ef1073b45789df95c6792bd3f25b02ed
Chuks <ikedichukwubuikem@gmail.com>
2023-10-07 17:03:27 +0100

    2.60

--- py_client/create.py
+++ py_client/create.py
@@ -1,5 +1,8 @@
 import requests
 
+headers = {'Authorization': 'Bearer af872ee22519a9b46fc3e57d061c06f27bb7aa54'}
+
+
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
@@ -8,6 +11,6 @@ data = {
 }
 
 
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 
 print(get_response.json())
