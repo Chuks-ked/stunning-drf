617d7698d15787fb17f9b9c52c287ea78d6ad68e
Chuks <ikedichukwubuikem@gmail.com>
2023-10-07 16:14:30 +0100

    2.46

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,13 +4,14 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.http import Http404
 from .models import Product
+from .permissions import isStaffEditorPermission
 from .serializers import ProductSerializer
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.DjangoModelPermissions]
+    permission_classes = [permissions.IsAdminUser, isStaffEditorPermission]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
