16f7ebc7297925a54555e9c16385467933c0440b
Chuks <ikedichukwubuikem@gmail.com>
2023-10-11 08:09:13 +0100

    request

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
+from .validators import validate_title_no_hello, unique_product_title
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
@@ -9,20 +10,30 @@ class ProductSerializer(serializers.ModelSerializer):
         view_name='product-detail',
         lookup_field='pk',
     )
-    email = serializers.EmailField(write_only=True)
+    # email = serializers.EmailField(write_only=True)
+    title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
+    name = serializers.CharField(source='title', read_only=True)
     class Meta:
         model = Product
         fields = [
             'url',
             'edit_url',
-            'email',
             'id',
             'title',
+            # 'name',
             'content',
             'price',
             'sale_price',
             'my_discount',
         ]
+
+    # def validate_title(self, value):
+    #     request = self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
 
     # def create(self, validated_data):
     #     # return Product.object.create(**validated_data)
