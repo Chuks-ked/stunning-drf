9b2dc8918e10b835223637d0dde18f9fd333f0dc
Chuks <ikedichukwubuikem@gmail.com>
2023-10-11 07:31:20 +0100

    custom

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -9,11 +9,13 @@ class ProductSerializer(serializers.ModelSerializer):
         view_name='product-detail',
         lookup_field='pk',
     )
+    email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
         fields = [
             'url',
             'edit_url',
+            'email',
             'id',
             'title',
             'content',
@@ -21,6 +23,19 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount',
         ]
+
+    # def create(self, validated_data):
+    #     # return Product.object.create(**validated_data)
+    #     # email =  validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     # print(email, obj)
+    #     return obj
+
+    # def update(self, instance, validated_data):
+    #     email = validated_data.pop('email')
+    #     return super().update(instance, validated_data)
+        # instance.title = validated_data.get('title')
+        # return instance
 
     def get_edit_url(self, obj):
         request = self.context.get('request')
--- backend/products/views.py
+++ backend/products/views.py
@@ -17,12 +17,16 @@ class ProductListCreateAPIView(
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
+        # email = serializer.validated_data.pop('email')
+        # print(email)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content')
         None
         if content is None:
             content = title
         serializer.save(content=content)
+
+    
 
 class ProductDetailAPIView(
     StaffEditorPermissionMixin,
